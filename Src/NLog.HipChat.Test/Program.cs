13460215e1a76c7c5161107329e22f3f396e98e1
Kirk Chen <rwk0119@yahoo.com.tw>
2014-03-14 11:16:02 +0800

    Inistal Nlog.HipChat Project

---
+++ Src/NLog.HipChat.Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLg.HipChat.Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+            
+            logger.Debug("This is Debug");
+            logger.Info("This is Info");
+            logger.Warn("This is Warn");
+            logger.Error("This is Error");
+            logger.Fatal("This is Fatal");
+
+            Console.WriteLine("Finish!");
+            Console.Read();
+        }
+    }
+}
---
+++ Src/NLog.HipChat/HipChatTarget.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using NLog.Targets;
+using System.ComponentModel.DataAnnotations;
+using HipChat;
+using System.ComponentModel;
+
+namespace NLog.Targets.HipChat
+{
+    [Target("HipChat")]
+    public sealed class HipChatTarget : TargetWithLayout
+    {
+        public HipChatTarget()
+        {
+        }
+
+        [Required]
+        public string AuthToken { get; set; }
+
+        [Required]
+        public string RoomId { get; set; }
+
+        [DefaultValue("Nlog.HipChatTarget")]
+        public string SenderName { get; set; }
+
+        [DefaultValue(HipChatClient.BackgroundColor.yellow)]
+        public HipChatClient.BackgroundColor BackgroundColor { get; set; }
+
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            string log = this.Layout.Render(logEvent);
+
+            this.SendMessageToHipchat(log);
+        }
+
+        private void SendMessageToHipchat(string message)
+        {
+            var client = new HipChatClient(this.AuthToken, this.RoomId, this.SenderName);
+
+            client.SendMessage(message, this.BackgroundColor);
+        }
+    }
+}
