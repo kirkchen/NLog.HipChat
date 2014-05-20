9e070fea7b48de1db151cd07b89c87d7cdded244
Kirk Chen <rwk0119@gmail.com>
2014-05-20 22:46:27 +0800

    add post build script, for packing nuget package

---
+++ Build/PublishNuGetPackage.sh
@@ -0,0 +1,9 @@
+VersionNumber="$VersionNumberPrefix.${TRAVIS_BUILD_NUMBER}"
+
+echo "VersionNumber: $VersionNumber"
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then	
+	mkdir Src/Nuget/Packages
+
+	mono --runtime=v4.0.30319 Src/.nuget/NuGet.exe Pack Src/Nuget/Nlog.HipChat/Nlog.HipChat.nuspec -NonInteractive -OutputDirectory Src/Nuget/Packages
+fi
