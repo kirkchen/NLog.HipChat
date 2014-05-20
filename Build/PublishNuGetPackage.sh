624db405fecdda4287b06e54285bf7c916b1b2c2
Kirk Chen <rwk0119@gmail.com>
2014-05-20 23:06:32 +0800

    Add version number for nuget package

--- Build/PublishNuGetPackage.sh
+++ Build/PublishNuGetPackage.sh
@@ -5,5 +5,5 @@ echo "VersionNumber: $VersionNumber"
 if [ "$TRAVIS_BRANCH" == "master" ]; then	
 	mkdir Src/Nuget/Packages
 
-	mono --runtime=v4.0.30319 Src/.nuget/NuGet.exe Pack Src/Nuget/Nlog.HipChat/NLog.HipChat.nuspec -NonInteractive -OutputDirectory Src/Nuget/Packages
+	mono --runtime=v4.0.30319 Src/.nuget/NuGet.exe Pack Src/Nuget/Nlog.HipChat/NLog.HipChat.nuspec -NonInteractive -Version ${$VersionNumber}
 fi
