66533ecc1aae787976fb5e75e8a7fbd4410ffb02
Kirk Chen <rwk0119@gmail.com>
2014-05-21 00:16:29 +0800

    fix paramter type

--- Build/PublishNuGetPackage.sh
+++ Build/PublishNuGetPackage.sh
@@ -4,5 +4,5 @@ echo "VersionNumber: $VersionNumber"
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then		
 	mono --runtime=v4.0.30319 Src/.nuget/NuGet.exe Pack NLog.HipChat.nuspec -NonInteractive -Version ${VersionNumber}
-	mono --runtime=v4.0.30319 Src/.nuget/NuGet.exe Push NLog.HipChat.${VersionNumber}.nupkg ${NUGET_APIKEY} -NonInteractive
+	mono --runtime=v4.0.30319 Src/.nuget/NuGet.exe Push NLog.HipChat.${VersionNumber}.nupkg $NUGET_APIKEY -NonInteractive	
 fi
